42ce01005006f128eae0c3ea744bc6fcc9d50c8b
Jeremy Huot <logflume@live.com>
2020-07-01 19:39:32 +0000

    Updated input.js to map 'wasd' to write move commands for the snake

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -7,6 +12,18 @@ const setupInput = function() {
   stdin.on('data', function handleUserInput(key) {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");
+    }
+    if (key === 's') {
+      conn.write("Move: down");
+    }
+    if (key === 'd') {
+      conn.write("Move: right");
     }
   });
 
