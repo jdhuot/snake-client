fcd2537b96177e6d8682cd99a4a494e36df83e11
Jeremy Huot <logflume@live.com>
2020-07-01 15:57:32 +0000

    Refactored play.js to pull the connect function from client.js

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  // message when we get booted from server for idling
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
